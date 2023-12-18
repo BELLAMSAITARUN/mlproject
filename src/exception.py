61ae338f86358a4da2d4e5cf88095edb1b469b0d
BELLAMSAITARUN <19211a0310@bvrit.ac.in>
2023-12-18 20:59:50 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 #sys provides various function and variables thata are used to manipulate different parts of the python runtime environment
 import sys
-import logging 
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -16,3 +16,4 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_message_detail=error_detail)
     def __str__(self):
         return self.error_message
+    
